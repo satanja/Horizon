5c28f5a03976400c00a5b5a9e950d1e8371158b6
satanja <s.a.tanja@student.tue.nl>
2018-08-21 13:59:12 +0200

    Add scraper call to bot

--- Discord bot/bot.py
+++ Discord bot/bot.py
@@ -1,7 +1,11 @@
 import discord
 from discord.ext import commands
 
+from scraper import getInfo
+
+#Constants
 TOKEN = 'NDgxMDY4NDgyMTkyOTk4NDAw.DlxAtw.bIsvvp5ZDNdQBZ72O0GpdNZnN_U'
+TEAM = "Team Horizon"
 
 client = commands.Bot(command_prefix = '!')
 
@@ -11,15 +15,15 @@ async def on_ready():
 
 @client.command()
 async def pro():
-  await client.say('pro')
+  await client.say(getInfo(1, TEAM))
 
 @client.command()
 async def semi():
-  await client.say('semi-pro')
+  await client.say(getInfo(2, TEAM))
 
 @client.command()
 async def amateur():
-  await client.say('amateur')
+  await client.say(getInfo(3, TEAM))
 
 
 client.run(TOKEN)
