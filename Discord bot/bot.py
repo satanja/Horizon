21b93cc1ef1bd1a9a6a2af04ae35465f07b1ca2f
satanja <s.a.tanja@student.tue.nl>
2018-08-21 19:48:17 +0200

    fixup

--- Discord bot/bot.py
+++ Discord bot/bot.py
@@ -1,10 +1,9 @@
 import discord
 from discord.ext import commands
-
 from scraper import getInfo
+import tokenfile
 
 #Constants
-TOKEN = 'NDgxMDY4NDgyMTkyOTk4NDAw.DlxAtw.bIsvvp5ZDNdQBZ72O0GpdNZnN_U'
 TEAM = "Team Horizon"
 
 client = commands.Bot(command_prefix = '!')
@@ -26,4 +25,4 @@ async def amateur():
   await client.say(getInfo(3, TEAM))
 
 
-client.run(TOKEN)
+client.run(tokenfile.TOKEN)
